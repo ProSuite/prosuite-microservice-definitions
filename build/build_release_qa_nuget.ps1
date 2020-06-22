b00cf01ab12525f18773d51225481f62c6411709
Emanuel Mahler <e.mahler@esri.ch>
2020-06-22 17:06:09 +0200

    Add basic build scripts

---
+++ build/build_release_qa_nuget.ps1
@@ -0,0 +1,24 @@
+# Builds the QA service definitions and packs them into a nupkg
+# The relevant output is
+# - ProSuite.Microservices.Definitions.Shared.*.nupkg
+# - ProSuite.Microservices.Definitions.QA.*.nupkg
+
+# Note: The very large native libraries are excluded. They will be provided by the gRPC nuget package.
+
+#
+# Make sure nuget.exe is in the PATH
+#
+
+cd $PSScriptRoot
+
+mode con:cols=200 lines=15000
+
+dotnet build ..\src\ProSuite.Microservices.Definitions.Shared\ProSuite.Microservices.Definitions.Shared.csproj -property:Configuration=Release
+dotnet build ..\src\ProSuite.Microservices.Definitions.QA\ProSuite.Microservices.Definitions.QA.csproj -property:Configuration=Release
+
+# exclude the native binaries, otherwise the package would become huge (https://stackoverflow.com/questions/41941588/binaries-are-added-to-project-when-nuget-package-has-grpc-core-as-a-dependency)
+nuget pack ..\src\ProSuite.Microservices.Definitions.Shared\ProSuite.Microservices.Definitions.Shared.csproj -Exclude '**\*.x86.*;**\*.x64.*' -OutputDirectory .\output -Properties Configuration=Release
+nuget pack ..\src\ProSuite.Microservices.Definitions.QA\ProSuite.Microservices.Definitions.QA.csproj -Exclude '**\*.x86.*;**\*.x64.*' -OutputDirectory .\output -Properties Configuration=Release
+
+
+pause
