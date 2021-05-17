f139fd503fe0cb2aedc1f674e7c781c892eab6cf
Emanuel Mahler <e.mahler@esri.ch>
2021-05-17 10:18:12 +0200

    Add experimental quality test service

--- build/build_release_qa_dlls.ps1
+++ build/build_release_qa_dlls.ps1
@@ -10,5 +10,6 @@ mode con:cols=200 lines=15000
 
 dotnet build ..\src\ProSuite.Microservices.Definitions.Shared\ProSuite.Microservices.Definitions.Shared.csproj -property:Configuration=Release -o .\output
 dotnet build ..\src\ProSuite.Microservices.Definitions.QA\ProSuite.Microservices.Definitions.QA.csproj -property:Configuration=Release -o .\output
+dotnet build ..\src\ProSuite.Microservices.Definitions.QA.Test\ProSuite.Microservices.Definitions.QA.Test.csproj -property:Configuration=Release -o .\output
 
 pause
