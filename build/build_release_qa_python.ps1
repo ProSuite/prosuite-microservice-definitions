c524fb5d6b6b6b851300dee958513844c5a98c9b
Stefan Schlaefli <s.schlaefli@esri.ch>
2021-09-28 16:24:18 +0200

    compile protos to python code

---
+++ build/build_release_qa_python.ps1
@@ -0,0 +1,14 @@
+# Builds the QA service definitions to the output directory
+# The relevant output is
+# - ProSuite.Microservices.Definitions.Shared.*
+# - ProSuite.Microservices.Definitions.QA.*
+# These python files can be checked into the consuming repo's lib folder.
+
+cd $PSScriptRoot
+
+mode con:cols=200 lines=15000
+
+python -m grpc.tools.protoc --proto_path=../src/protos --python_out=output --grpc_python_out=output quality_verification_service.proto
+python -m grpc.tools.protoc --proto_path=../src/protos --python_out=output shared_types.proto
+
+pause
