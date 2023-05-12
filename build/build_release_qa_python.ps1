d062e2a0e46e00c91c30b7b6595b8f6303b70ceb
Emanuel Mahler <e.mahler@dirageosystems.ch>
2023-05-12 20:01:38 +0200

    Add missing commands in python generation

--- build/build_release_qa_python.ps1
+++ build/build_release_qa_python.ps1
@@ -10,5 +10,7 @@ mode con:cols=200 lines=15000
 
 python -m grpc.tools.protoc --proto_path=../src/protos --python_out=output --grpc_python_out=output quality_verification_service.proto
 python -m grpc.tools.protoc --proto_path=../src/protos --python_out=output shared_types.proto
+python -m grpc.tools.protoc --proto_path=../src/protos --python_out=output shared_qa.proto
+python -m grpc.tools.protoc --proto_path=../src/protos --python_out=output --grpc_python_out=output quality_test.proto
 
 pause
