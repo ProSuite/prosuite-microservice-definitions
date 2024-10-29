920d5146f7786b053b3594198c4245a9889ea3dc
Emanuel Mahler <e.mahler@dirageosystems.ch>
2024-10-29 19:01:17 +0100

    Remove shared_types, replace with new protos

--- build/build_release_qa_python.ps1
+++ build/build_release_qa_python.ps1
@@ -12,8 +12,9 @@ mkdir $OutputDir
 
 python -m grpc.tools.protoc --proto_path=../src/protos --python_out=$OutputDir --pyi_out=$OutputDir --grpc_python_out=$OutputDir quality_verification_service.proto
 python -m grpc.tools.protoc --proto_path=../src/protos --python_out=$OutputDir --pyi_out=$OutputDir --grpc_python_out=$OutputDir quality_verification_ddx.proto
-python -m grpc.tools.protoc --proto_path=../src/protos --python_out=$OutputDir --pyi_out=$OutputDir shared_types.proto
 python -m grpc.tools.protoc --proto_path=../src/protos --python_out=$OutputDir --pyi_out=$OutputDir shared_qa.proto
+python -m grpc.tools.protoc --proto_path=../src/protos --python_out=$OutputDir --pyi_out=$OutputDir shared_gdb.proto
+python -m grpc.tools.protoc --proto_path=../src/protos --python_out=$OutputDir --pyi_out=$OutputDir shared_ddx.proto
 python -m grpc.tools.protoc --proto_path=../src/protos --python_out=$OutputDir --pyi_out=$OutputDir --grpc_python_out=$OutputDir quality_test.proto
 
 Write-Host "Created python output in ${OutputDir}"
