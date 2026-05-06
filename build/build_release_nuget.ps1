127c2361c229ea80f694b510fffc97106bd7cd52
Emanuel Mahler <e.mahler@dirageosystems.ch>
2026-05-06 23:59:15 +0200

    Upgrade nuspec build to dotnet pack

--- build/build_release_nuget.ps1
+++ build/build_release_nuget.ps1
@@ -3,32 +3,29 @@
 # - ProSuite.Microservices.Definitions.Shared.*.nupkg
 # - ProSuite.Microservices.Definitions.QA.*.nupkg
 
-# Note: The very large native libraries are excluded. They will be provided by the gRPC nuget package.
-
-#
-# Make sure nuget.exe is in the PATH
-#
-
 cd $PSScriptRoot
 
 mode con:cols=200 lines=15000
 
 # QA:
+Write-Host "Building and packing DiraGeoSystems.ProSuite.Microservices.QA..."
 dotnet build ..\src\ProSuite.Microservices.Definitions.QA\ProSuite.Microservices.Definitions.QA.csproj -property:Configuration=Release
-
-# exclude the native binaries, otherwise the package would become huge (https://stackoverflow.com/questions/41941588/binaries-are-added-to-project-when-nuget-package-has-grpc-core-as-a-dependency)
-nuget pack ..\src\ProSuite.Microservices.Definitions.QA\ProSuite.Microservices.Definitions.QA.csproj -Exclude '**\*.x86.*;**\*.x64.*' -OutputDirectory .\output\nuget -Properties Configuration=Release
-
+dotnet pack ..\src\ProSuite.Microservices.Definitions.QA\ProSuite.Microservices.Definitions.QA.csproj -o .\output\nuget -c Release
+Write-Host "-------------------"
+Write-Host
 
 # QA.Test:
+Write-Host "Building and packing DiraGeoSystems.ProSuite.Microservices.QA.Test..."
 dotnet build ..\src\ProSuite.Microservices.Definitions.QA.Test\ProSuite.Microservices.Definitions.QA.Test.csproj -property:Configuration=Release
-
-# exclude the native binaries, otherwise the package would become huge (https://stackoverflow.com/questions/41941588/binaries-are-added-to-project-when-nuget-package-has-grpc-core-as-a-dependency)
-nuget pack ..\src\ProSuite.Microservices.Definitions.QA.Test\ProSuite.Microservices.Definitions.QA.Test.csproj -Exclude '**\*.x86.*;**\*.x64.*' -OutputDirectory .\output\nuget -Properties Configuration=Release
+dotnet pack ..\src\ProSuite.Microservices.Definitions.QA.Test\ProSuite.Microservices.Definitions.QA.Test.csproj -o .\output\nuget -c Release
+Write-Host "-------------------"
+Write-Host
 
 # Geometry:
+Write-Host "Building and packing DiraGeoSystems.ProSuite.Microservices.Geometry..."
 dotnet build ..\src\ProSuite.Microservices.Definitions.Geometry\ProSuite.Microservices.Definitions.Geometry.csproj -property:Configuration=Release
-nuget pack ..\src\ProSuite.Microservices.Definitions.Geometry\ProSuite.Microservices.Definitions.Geometry.csproj -Exclude '**\*.x86.*;**\*.x64.*' -OutputDirectory .\output\nuget -Properties Configuration=Release
-
+dotnet pack ..\src\ProSuite.Microservices.Definitions.Geometry\ProSuite.Microservices.Definitions.Geometry.csproj -o .\output\nuget -c Release
+Write-Host "-------------------"
+Write-Host
 
 pause
